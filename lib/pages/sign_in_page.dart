e370f109392fe091d55be77fd2bc326d40c7fdea
ahmdxhabibi <ahmdxhabibi@gmail.com>
2022-09-21 20:58:42 +0700

    Update sign_in_page.dart

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:space/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          title(),
+        ],
+      ),
+    );
+  }
+
+  Widget title() {
+    return Container(
+      margin: EdgeInsets.only(top: 84),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login to your\naccount',
+            style:
+                blackAccentTextStyle.copyWith(fontSize: 24, fontWeight: bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 87,
+                height: 4,
+                margin: EdgeInsets.only(right: 4),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4), color: blackColor),
+              ),
+              Container(
+                width: 8,
+                height: 4,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4), color: blackColor),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
